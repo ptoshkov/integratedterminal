b5c85102c3e5e5b2eadadd449769d658217e6dd9
Pete Toshkov <petetoshkov@gmail.com>
2026-03-31 21:23:03 +0300

    Added profiles

--- integratedterminal/+controller/integratedterminalpreferences.m
+++ integratedterminal/+controller/integratedterminalpreferences.m
@@ -1,58 +1,73 @@
-classdef integratedterminalpreferences
+classdef integratedterminalpreferences < handle
     %INTEGRATEDTERMINALPREFERENCES Summary of this class goes here
     %   Detailed explanation goes here
 
     properties
-        cd
+        destdir
         json
-        help
     end
 
     methods
-        function obj = integratedterminalpreferences()
+        function obj = integratedterminalpreferences(profile)
             %INTEGRATEDTERMINALPREFERENCES Construct an instance of this class
             %   Detailed explanation goes here
-            [obj.cd, ~] = fileparts(mfilename("fullpath"));
-            obj.json = obj.cd + "/preferences.json";
-            obj.help = obj.cd + "/help.txt";
-
-            if ~isfile(obj.json)
-                obj.reset();
+            arguments
+                profile = []
             end
+
+            [cd, ~] = fileparts(mfilename("fullpath"));
+            obj.destdir = cd + "/../profiles";
+            default = obj.destdir + "/default.json";
+
+            %% Create the profiles directory if it doesn't exist
+            if ~isfolder(obj.destdir)
+                mkdir(obj.destdir);
+            end
+
+            %% Create the default profile if it doesn't exist
+            if ~isfile(default)
+                if computer == "PCWIN64"
+                    copyfile(cd + "/defaultwindows.json", default);
+                elseif computer == "GLNXA64"
+                    copyfile(cd + "/defaultlinux.json", default);
+                elseif computer == "MACI64" || computer == "MACA64"
+                    copyfile(cd + "/defaultmacos.json", default);
+                else
+                    error('Unsupported operating system.');
+                end
+            end
+
+            if isempty(profile)
+                return;
+            end
+
+            obj.load(profile);
         end
 
-        function reset(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            if computer == "PCWIN64"
-                copyfile(obj.cd + "/defaultwindows.json", obj.json);
-            elseif computer == "GLNXA64"
-                copyfile(obj.cd + "/defaultlinux.json", obj.json);
-            elseif computer == "MACI64" || computer == "MACA64"
-                copyfile(obj.cd + "/defaultmacos.json", obj.json);
+        function delete(obj)
+            delete(obj.json);
+        end
+
+        function load(obj, profile)
+            %% Copy the profile and strip the comments,
+            %% because comments are not allowed in JSON
+            jsonwithcomments = fileread(obj.destdir + "/" + profile);
+            [jsonwithoutcomments, ~] = regexp(jsonwithcomments, "//[^\r\n]*", "split");
+            tmp = tempname();
+            writecell(jsonwithoutcomments', tmp + ".txt", "QuoteStrings", "none");
+            movefile(tmp + ".txt", tmp + ".json");
+            obj.json = tmp + ".json";
+        end
+
+        function open(obj)
+            [file, location] = uigetfile("*.json", "", obj.destdir);
+
+            if isequal(file,0)
+                % User selected Cancel
+                return;
             else
-                error('Unsupported operating system.');
+                open(fullfile(location, file));
             end
-        end
-
-        function txt = read(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            txt = fileread(obj.json);
-        end
-
-        function txt = gethelp(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            txt = fileread(obj.help);
-        end
-
-        function write(obj, txtCell)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            tmp = tempname + ".txt";
-            writecell(txtCell, tmp, "QuoteStrings", false);
-            movefile(tmp, obj.json);
         end
     end
 end
